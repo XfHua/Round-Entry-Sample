c22734ccbaffb07e606a735e55e3fa0649058f1d
XfHua <jasonHuaxf@163.com>
2020-07-15 10:39:20 +0800

    Add files via upload

---
+++ Xamarin_Forms___CarouselPage/CarouselPageNavigation/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace CarouselPageNavigation
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+
+	public class RoundedEntry : Entry
+	{
+	}
+}
+
---
+++ Xamarin_Forms___CarouselPage/Droid/RoundedEntryRendererAndroid.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using CarouselPageNavigation;
+using CarouselPageNavigation.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(RoundedEntry), typeof(RoundedEntryRendererAndroid))]
+namespace CarouselPageNavigation.Droid
+{
+    [Obsolete]
+    public class RoundedEntryRendererAndroid : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null)
+            {
+                //Control.SetBackgroundResource(Resource.Layout.rounded_shape);
+                var gradientDrawable = new GradientDrawable();
+                gradientDrawable.SetCornerRadius(60f);
+                gradientDrawable.SetStroke(5, Android.Graphics.Color.DeepPink);
+                gradientDrawable.SetColor(Android.Graphics.Color.LightGray);
+                Control.SetBackground(gradientDrawable);
+
+                Control.SetPadding(50, Control.PaddingTop, Control.PaddingRight,
+                    Control.PaddingBottom);
+            }
+        }
+    }
+}
